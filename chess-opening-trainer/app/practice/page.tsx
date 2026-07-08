2fcd3e510ac01fdf03c4329d43b34d214ede7b47
Purunjay Kandwal <purunjaykandwal14@gmail.com>
2026-07-08 13:31:24 +0530

    Milestone 2: Added interactive chessboard

--- chess-opening-trainer/app/practice/page.tsx
+++ chess-opening-trainer/app/practice/page.tsx
@@ -1,19 +1,48 @@
+"use client";
+
+import { useState } from "react";
+import { Chess } from "chess.js";
+import { Chessboard } from "react-chessboard";
+
 export default function PracticePage() {
+  const [game, setGame] = useState(() => new Chess());
+
+  function onDrop({
+    sourceSquare,
+    targetSquare,
+  }: {
+    sourceSquare: string;
+    targetSquare: string;
+  }) {
+    const copy = new Chess(game.fen());
+
+    try {
+      const move = copy.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: "q",
+      });
+
+      if (!move) return false;
+
+      setGame(copy);
+
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
   return (
     <main className="min-h-screen bg-slate-900 flex items-center justify-center">
-
-      <div className="text-center text-white">
-
-        <h1 className="text-5xl font-bold">
-          Practice Page
-        </h1>
-
-        <p className="mt-6 text-xl">
-          Chessboard coming soon...
-        </p>
-
+      <div style={{ width: 600 }}>
+        <Chessboard
+          options={{
+            position: game.fen(),
+            onPieceDrop: onDrop,
+          }}
+        />
       </div>
-
     </main>
   );
 }
